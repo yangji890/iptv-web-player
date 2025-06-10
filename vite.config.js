79ae63b0aadc4a625aed366a896e1fdf724926a6
yangji890 <oyjj6w@hotmail.com>
2025-06-11 01:57:50 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',  // 指定项目根目录
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
+  server: {
+    port: 3000
   }
 })
